23c9d1b23e1a507284db95c37572acf9eea4c2f5
Lukas Stratmann <stratmann@ccs-labs.org>
2024-03-17 18:55:16 +0100

    sequence timing

--- bpy-load-videos.py
+++ bpy-load-videos.py
@@ -61,10 +61,16 @@ def blender_main():
         required=True,
     )
     parser.add_argument(
-        '--context-time',
+        '--context-before',
         type=float,
-        help="Seconds of context before and after a beep.",
-        default=7,
+        help="Seconds of context before a beep.",
+        default=14,
+    )
+    parser.add_argument(
+        '--context-after',
+        type=float,
+        help="Seconds of context after a beep.",
+        default=5,
     )
     args = parser.parse_args()
 
@@ -89,26 +95,82 @@ def blender_main():
     bpy.context.scene.render.fps = math.ceil(fps)
     bpy.context.scene.render.fps_base = math.ceil(fps) / fps
 
+    i_incident = 0
     for video_filename in sorted(incident_times_by_video.keys()):
         for beep_time in incident_times_by_video[video_filename]:
+            print(f"Adding incident {video_filename=}, {beep_time=} s")
             add_incident_to_timeline(
                 video_filename=video_filename,
                 beep_time=beep_time,
-                context_time=args.context_time,
+                context_before=args.context_before,
+                context_after=args.context_after,
+                start_frame = int(
+                    i_incident
+                    * (args.context_before + args.context_after)
+                    * fps
+                ),
+                fps=fps,
             )
+            i_incident += 1
+
+    bpy.context.scene.frame_end = int(
+        i_incident
+        * (args.context_before + args.context_after)
+        * fps
+    )
+
+    # Update view in sequence editor
+    # - Set frame range to include all strips
+    # - Zoom out to show all
+    # TODO: doesn't work
+
+    # screen = bpy.data.screens['Video Editing']
+    # print(f"{bpy.context.screen=}")
+    # for area in screen.areas:
+    #     if area.type == "SEQUENCE_EDITOR":
+    #         override = bpy.context.copy()
+    #         # change context to the sequencer
+    #         override["screen"] = screen
+    #         override["area"] = area
+    #         override["region"] = area.regions[-1]
+    #         # run the command with the correct context
+    #         with bpy.context.temp_override(**override):
+    #             bpy.ops.sequencer.set_range_to_strips()
+    #             bpy.ops.sequencer.view_all()
+    #         break
 
 
 def add_incident_to_timeline(
     video_filename: str,
     beep_time: float,
-    context_time: float,
+    context_before: float,
+    context_after: float,
+    start_frame: int,
+    fps: float,
 ):
-    bpy.context.scene.sequence_editor.sequences.new_movie(
+    v_sequence = bpy.context.scene.sequence_editor.sequences.new_movie(
         name=video_filename,
         filepath=video_filename,  # TODO: path?
-        frame_start=0,  # TODO
+        frame_start=start_frame,
         channel=0,
     )
+    a_sequence = bpy.context.scene.sequence_editor.sequences.new_sound(
+        name=video_filename,
+        filepath=video_filename,  # TODO: path?
+        frame_start=start_frame,
+        channel=0,
+    )
+
+    print(f"new {v_sequence=}")
+    frame_start = start_frame - (beep_time - context_before) * fps
+    frame_offset_start = (beep_time - context_before) * fps
+    frame_final_duration = int((context_before + context_after) * fps)
+    v_sequence.frame_start = frame_start
+    v_sequence.frame_offset_start = frame_offset_start
+    v_sequence.frame_final_duration = frame_final_duration
+    a_sequence.frame_start = frame_start
+    a_sequence.frame_offset_start = frame_offset_start
+    a_sequence.frame_final_duration = frame_final_duration
     # if beep_time < context_time:
     #     # TODO: check if previous video file exists
     #     pass
@@ -126,7 +188,7 @@ def get_exif(
         stdout=subprocess.PIPE,
     )
     metrics = dict()
-    for line in exiftool_process.stdout:
+    for line in exiftool_process.stdout.readlines():
         k, v = str(line).split(':', maxsplit=1)
         metrics[k.strip()[2:]] = v.strip()[:-3]
         # '[2:] is for removing the "b'" at the start
